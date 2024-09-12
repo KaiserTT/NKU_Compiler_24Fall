83b093e2ba9198973e020156f06a05f524dd79fd
Tengjiao Yin <60817795+Ethan2k04@users.noreply.github.com>
2024-09-12 10:04:13 +0800

    Delete hello_rust/target directory

--- hello_rust/target/debug/deps/hello_rust-41045ef933216ce3.d
+++
@@ -1,5 +0,0 @@
-D:\MyCode\rust\hello_rust\target\debug\deps\libhello_rust-41045ef933216ce3.rmeta: src/main.rs
-
-D:\MyCode\rust\hello_rust\target\debug\deps\hello_rust-41045ef933216ce3.d: src/main.rs
-
-src/main.rs:
--- hello_rust/target/debug/deps/hello_rust-811ea7255a911dac.d
+++
@@ -1,5 +0,0 @@
-D:\MyCode\rust\hello_rust\target\debug\deps\libhello_rust-811ea7255a911dac.rmeta: src/main.rs
-
-D:\MyCode\rust\hello_rust\target\debug\deps\hello_rust-811ea7255a911dac.d: src/main.rs
-
-src/main.rs:
