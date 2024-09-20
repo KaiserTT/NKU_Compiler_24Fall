7391f1148c1a187f9ba0ad4fe688dc9565c18b8f
Ethan2k04 <3287816638@qq.com>
2024-09-21 00:20:15 +0800

    upload riscv sample code 3

--- asm_riscv64/README.md
+++ asm_riscv64/README.md
@@ -9,3 +9,4 @@
 # 代码目录
 1. `hello_world.s`：实现了打印"Hello Compiler"的功能
 2. `factorial.s`：实现了接收用户输入，输出阶乘的功能
+3. `fibonacci.s`：实现了接收用户输入迭代次数，输出Fibonacci数列结果的功能
---
+++ asm_riscv64/fibonacci.s
@@ -0,0 +1,73 @@
+    .file "fibonacci.c"
+    .option nopic
+    .attribute arch, "rv64i2p1_m2p0_a2p1_f2p2_d2p2_c2p0_zicsr2p0"
+    .attribute unaligned_access, 0
+    .attribute stack_align, 16
+    .text
+    .align 1
+    .globl fibonacci
+    .type fibonacci, @function
+fibonacci:
+    li a1, 0
+    li a2, 1
+    beq a0, zero, .L2
+    li a3, 2
+.L1:
+    beq a0, a3, .L2
+    add a4, a1, a2
+    mv a1, a2
+    mv a2, a4
+    addi a3, a3, 1
+    j .L1
+.L2:
+    mv a0, a2
+    ret
+    .size fibonacci, .-fibonacci
+
+    .section .rodata.str1.8,"aMS",@progbits,1
+    .align 3
+.LC0:
+    .string "%d"
+    .align 3
+.LC1:
+    .string "Fibonacci result is: %d\n"
+
+    .section .text.startup,"ax",@progbits
+    .align 1
+    .globl main
+    .type main, @function
+main:
+    addi sp, sp, -32
+    sd s0, 16(sp)
+    lui s0, %hi(n)
+    lui a0, %hi(.LC0)
+    addi a0, a0, %lo(.LC0)
+    addi a1, s0, %lo(n)
+    sd ra, 24(sp)
+    call __isoc99_scanf
+
+    lw a0, %lo(n)(s0)
+    call fibonacci
+
+    mv a1, a0
+    lui a0, %hi(.LC1)
+    addi a0, a0, %lo(.LC1)
+    call printf
+
+    ld ra, 24(sp)
+    ld s0, 16(sp)
+    li a0, 0
+    addi sp, sp, 32
+    jr ra
+    .size main, .-main
+
+    .globl n
+    .section .sbss,"aw",@nobits
+    .align 2
+    .type n, @object
+    .size n, 4
+n:
+    .zero 4
+
+    .ident "GCC: () 12.2.0"
+    .section .note.GNU-stack,"",@progbits
